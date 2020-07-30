3322d42d78dc3812b0ebf7f6ef0b8e6a5f532af0
Saurabh <Saurabh@mx>
2020-07-30 18:04:29 +0530

    initial

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <Navbar></Navbar>
+    <v-card style="margin-top: 4.5rem;" class="mx-auto" width="40%">
+      <v-card-title>Hello</v-card-title></v-card
+    >
+  </v-app>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+export default {
+  components: {
+    Navbar,
+  },
+  props: {
+    source: String,
+  },
+  data: () => ({}),
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+   <v-app-bar class="toolbar" color="black" flat fixed>
+      <strong
+          ><h2><span class="title">zo<span style="color:red;">yu</span></span></h2></strong
+        >
+        <v-spacer></v-spacer>
+     <v-app-bar-nav-icon color="red"  @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+        <v-navigation-drawer
+      v-model="drawer"
+      app
+      right
+      temporary
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer></div>  
+    
+    
+</template>
+
+<script>
+export default {
+  data: () => ({
+      drawer: null,
+    }),
+}
+</script>
+
+<style scoped>
+.title {
+  display: inline;
+  color: #ffffff;
+  font-family: 'Nanum Gothic', sans-serif;
+}
+
+</style>
